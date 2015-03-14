1047d122d71e340814359a843c47b0d5c6bd2a4a
Marc Vouve <mvouve@gmail.com>
2015-03-14 16:11:38 -0700

    Added IP and host info

--- Client/app/src/main/java/com/ttwin/client/XMLHandler.java
+++ Client/app/src/main/java/com/ttwin/client/XMLHandler.java
@@ -3,14 +3,19 @@ package com.ttwin.client;
 
 import android.content.Context;
 import android.location.Location;
+import android.net.wifi.WifiManager;
+import android.text.format.Formatter;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -31,12 +36,17 @@ public class XMLHandler {
 
     Element Entry;
     Element TimeElement;
-    Element GPS;
+    Element TimeNode;
+    Element GPSNode;
+    Element IdentNode;
+    Element InfoNode;
+    GPSHelper gpsHelp;
 
     private Context AppContext;
 
-    public XMLHandler(Context c)
+    public XMLHandler(Context c, GPSHelper help)
     {
+        gpsHelp = help;
         AppContext = c;
         DocFactory = DocumentBuilderFactory.newInstance();
         try
@@ -47,6 +57,21 @@ public class XMLHandler {
         {
             // nothing
         }
+
+        Entry = Doc.createElement("ENTRY");   // I don't know if the server's expecting from this level or DATA.
+        Doc.appendChild(Entry);
+
+        TimeNode = Doc.createElement("TIME");
+        Entry.appendChild(TimeNode);
+
+        GPSNode = Doc.createElement("GPS");
+        Entry.appendChild(GPSNode);
+
+        IdentNode = Doc.createElement("IDENT");
+        Entry.appendChild(IdentNode);
+
+        InfoNode = Doc.createElement("INFO");
+        Entry.appendChild(InfoNode);
 
     }
 
@@ -62,8 +87,7 @@ public class XMLHandler {
     {
         Doc = DocBuilder.newDocument();
 
-        Entry = Doc.createElement("ENTRY");   // I don't know if the server's expecting from this level or DATA.
-        Doc.appendChild(Entry);
+
 
         updateTime();
         updateGPS();
@@ -79,21 +103,87 @@ public class XMLHandler {
 
     public void updateGPS()
     {
-        GPSHelper gps = new GPSHelper(AppContext);
 
-        Location loc = gps.getLocation();
+        Map<String, Node> nodeMap = new HashMap<String, Node>();
+        Location loc = gpsHelp.getLocation();
+
+        nodeMap.put("LATITUDE", Doc.createElement("LATITUDE"));
+        nodeMap.put("LONGITUDE", Doc.createElement("LONGITUDE"));
+        nodeMap.put("ALTITUDE", Doc.createElement("ALTITUDE"));
+        nodeMap.put("SPEED", Doc.createElement("SPEED"));
+        nodeMap.put("HEADING", Doc.createElement("HEADING"));
+
+
+        nodeMap.get("LATITUDE").appendChild(Doc.createTextNode("" + loc.getLatitude()));
+        nodeMap.get("LONGITUDE").appendChild(Doc.createTextNode("" + loc.getLongitude()));
+        nodeMap.get("ALTITUDE").appendChild(Doc.createTextNode(("" + loc.getAltitude())));
+        nodeMap.get("SPEED").appendChild(Doc.createTextNode("" + loc.getSpeed()));
+        nodeMap.get("HEADING").appendChild(Doc.createElement("" + loc.getBearing()));
+
+        clearNode(GPSNode);
+
+        for(Node n : nodeMap.values())
+        {
+            GPSNode.appendChild(n);
+        }
     }
 
     public void updateTime()
     {
-        TimeElement = Doc.createElement("TIME");
-        TimeElement.appendChild(Doc.createTextNode());
+        clearNode(TimeElement);
+        TimeElement.appendChild(Doc.createTextNode(getTimeFormatted()));
         Doc.appendChild(TimeElement);
+    }
+
+    public void updateIdent()
+    {
+        WifiManager wm = (WifiManager) AppContext.getSystemService(Context.WIFI_SERVICE);
+        String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+
+        Map<String, Node> nodeMap = new HashMap<String, Node>();
+
+        nodeMap.put("IP", Doc.createElement("IP"));
+        nodeMap.put("HOSTNAME", Doc.createElement("HOSTNAME"));
+        nodeMap.put("MAC", Doc.createElement("MAC"));
+
+        nodeMap.get("IP").appendChild(Doc.createTextNode(Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress())));
+        nodeMap.get("HOSTNAME").appendChild(Doc.createTextNode("" + loc.getLongitude()));
+        nodeMap.get("MAC").appendChild(Doc.createTextNode(("" + loc.getAltitude())));
+
+
+    }
+
+    /**
+     * This method clears the nodes
+     *
+     * @date March 10, 2015
+     * @author Marc Vouve
+     * @designer Marc Vouve
+     *
+     * @param node node to clear
+     */
+    public void clearNode(Node node)
+    {
+        Node child;
+
+        while(node.hasChildNodes())
+        {
+            if(node.getFirstChild().hasChildNodes())
+            {
+                clearNode(node.getFirstChild());
+            }
+            else
+            {
+                node.removeChild(node.getFirstChild());
+            }
+        }
+
     }
 
     /**
      * Returns a formatted string of the current time and date.
      *
+     * @date March 10, 2015
      * @author Marc Vouve
      * @designer Marc Vouve
      *
